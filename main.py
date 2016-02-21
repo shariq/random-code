96fa5260a1b6fc6e86d319af6f225e57540a98de
Shariq Hashme <shariqhashme@gmail.com>
2016-02-21 11:34:44 -0500

    fixed some bugs

--- main.py
+++ main.py
@@ -6,33 +6,41 @@ import __builtin__
 def random_function(root = None):
     path = []
     ascend_probability = 0.9
-    if root is None:
-        root = eval(random.choice(dir(__builtin__)))
+    while root is None:
+        try:
+            root = eval(random.choice(dir(__builtin__)))
+        except:
+            continue
         while True:
             response = random_function(root)
             if response is not None:
                 return response
     if type(root) == type(random_function):
-        if random.random() < 0.8:
+        if random.random() < 0.95:
             return root
+    if random.random() < 0.8:
+        return None
     while True:
-        thingy = getattr(root, random.choice(dir(root)))
-        func = random_function(thingy)
-        if func is None:
+        try:
+            thingy = getattr(root, random.choice(dir(root)))
+        except:
             if random.random() > 0.9:
                 return None
             else:
                 continue
-        else:
-            if random.random() < 0.9:
-                return func
-            else:
+        func = random_function(thingy)
+        if func is None:
+            if random.random() > 0.5:
                 return None
+            else:
+                continue
+        else:
+            return func
 
-def pick_random_function(num_args):
+def pick_random_function(min_args, max_args):
     while True:
         func = random_function()
-        if len(inspect.getargspec(func)[0]) == num_args:
+        if min_args <= len(inspect.getargspec(func)[0]) <= max_args:
             return func
 
 class function_tree_leaf:
@@ -49,21 +57,23 @@ class function_tree_leaf:
         if self.level == 0:min_args = 2
         if self.level == 1:min_args = 1
         max_args = max([10-self.level,0])
-        num_args = random.randint(min_args, max_args)
-        func = pick_random_function(num_args)
+        func = pick_random_function(min_args, max_args)
         args = []
         for arg in range(num_args):
             retvalue = None
             while retvalue is None:
                 retvalue = function_tree_leaf(self).evaluate()
             args.append(retvalue)
+        print 'calling', func.__name__, 'with args', ' '.join(map(str,args))
         return func(*args)
     def evaluate(self):
         while True:
             try:
                 return self.evaluate_once()
             except:
+                pass
                 traceback.print_exc()
+                raw_input()
 
 if __name__ == '__main__':
     print function_tree_leaf().evaluate()
